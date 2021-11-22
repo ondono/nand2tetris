3ce3b0c7a8d10123bebe14ed6686968716722ebb
Xavi <xavi@ondono.com>
2021-11-22 13:13:39 +0100

    Adding the first Hack Assembly program

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,29 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+
+// SET R2=0
+
+
+@2
+M=0		//Load 0 into R2
+@1
+D=M		// Load R1 into D
+@END
+D;JEQ	// If R1 was 0, we're done
+		// otherwise we can proceed
+(beginLoop)
+@2
+D=M		// Load the accumulated value from R2
+@0
+D=D+M	// Add R0 to D
+@2
+M=D		// Copy the new D to R2
+@1
+M=M-1	// Decrement R1 by 1
+@1
+D=M		// Load R1 to D for comparing
+@beginLoop
+D; JNE	// if D (R1) is not 0, go back to beginLoop
+		// else, we're done
+(END)
